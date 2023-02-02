40f14d851421df1218333d87d9309c7c04c44b90
carson-loser <carson.loser@gmail.com>
2023-02-02 01:37:07 -0600

    small changes to controllers

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -2,31 +2,46 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
   getThoughts(req, res) {
-    Thought.find({})
+    Thought.find()
+      .then()
+    res.json()
+
   },
 
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
+      .then()
+    res.json
   },
 
   createThought(req, res) {
     Thought.create(req.body)
+      .then()
+    res.json
   },
 
   updateThought(req, res) {
     Thought.findOneAndUpdate({ _id: req.params.thoughtId })
+      .then()
+    res.json
   },
 
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then()
+    res.json
   },
 
   addReaction(req, res) {
     Thought.findOneAndUpdate({ _id: req.params.thoughtId })
+      .then()
+    res.json()
   },
 
   removeReaction(req, res) {
     Thought.findOneAndUpdate({ _id: req.params.thoughtId })
+      .then()
+    res.json()
   },
 }
 module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,10 +3,14 @@ const { User, Thought } = require('../models');
 const userController = {
   getUsers(req, res) {
     User.find({})
+      .then()
+    res.json()
   },
 
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
+      .then()
+    res.json()
   },
 
   createUser(req, res) {
@@ -14,11 +18,15 @@ const userController = {
   },
 
   updateUser(req, res) {
-    User.findOneAndUpdate({ _id: req.params.userId})
+    User.findOneAndUpdate({ _id: req.params.userId })
+      .then()
+    res.json()
   },
- 
+
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
+      .then()
+    res.json()
   },
 }
 module.exports = userController;
